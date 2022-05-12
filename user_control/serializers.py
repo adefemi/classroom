15fd121ef827197f12104a879ca85bfb35afb028
Adefemi Oseni <adefemigreat1995@gmail.com>
2022-05-12 15:05:28 +0100

    implemented user api

---
+++ user_control/serializers.py
@@ -0,0 +1,14 @@
+from .models import CustomUser
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = CustomUser
+        fields = "__all__"
+        
+        
+class CreateUserSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField()
